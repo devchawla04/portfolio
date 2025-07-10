4a69aba3c47dd92bb5ecbffdb609e19c08c6fce3
Dev Chawla <devchawla.04@gmail.com>
2025-07-10 15:28:14 +0530

    react downgrade

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,18 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const webpack = require('webpack');
+      config.plugins.push(
+        new webpack.IgnorePlugin({
+          resourceRegExp: /^canvas$/,
+        })
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
