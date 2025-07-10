1beac7e5b8f318b023793ebb759794f0da97b3fc
Dev Chawla <devchawla.04@gmail.com>
2025-07-10 14:46:24 +0530

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Devfolio | Portfolio Website using Next.js, Tailwind CSS, and Framer Motion",
-  description: "Devfolio is a portfolio website for developers to showcase their projects and skills.",
+  title: "DevChawla04 | Portfolio Website using Next.js, Tailwind CSS, and Framer Motion",
+  description: "DevChawla04 is a portfolio website for developers to showcase their projects and skills.",
 };
 
 export default function RootLayout({
